779af27d517a1034d21af9528b2f7b86aadcaade
Brandon Lecky <brandon@dreamincode.dev>
2024-12-22 03:56:54 -0800

    Add mock market data for TSLA and PLTR

--- src/app/components/Chart.tsx
+++ src/app/components/Chart.tsx
@@ -4,102 +4,12 @@ import { useEffect, useRef } from "react";
 import * as d3 from "d3";
 import axios from "axios";
 import { IChartConfig } from "../types/IChartConfig";
-
+import tslaRes from '../data/TSLA.json';
+import pltrRes from '../data/PLTR.json';
 
 interface IChartProps {
   chartConfig: IChartConfig;
 };
-
-const res = {
-  "Meta Data": {
-      "1. Information": "Intraday (60min) open, high, low, close prices and volume",
-      "2. Symbol": "TSLA",
-      "3. Last Refreshed": "2024-12-20 20:00:00",
-      "4. Interval": "60min",
-      "5. Output Size": "Compact",
-      "6. Time Zone": "US/Eastern"
-  },
-  "Time Series (60min)": {
-      "2024-12-20 20:00:00": {
-          "1. open": "522.67",
-          "2. high": "549.78",
-          "3. low": "518.34",
-          "4. close": "540.89",
-          "5. volume": "1234567"
-      },
-      "2024-12-20 19:00:00": {
-          "1. open": "501.32",
-          "2. high": "524.67",
-          "3. low": "497.23",
-          "4. close": "522.67",
-          "5. volume": "876543"
-      },
-      "2024-12-20 18:00:00": {
-          "1. open": "480.45",
-          "2. high": "510.78",
-          "3. low": "475.34",
-          "4. close": "501.32",
-          "5. volume": "765432"
-      },
-      "2024-12-20 17:00:00": {
-          "1. open": "493.12",
-          "2. high": "502.34",
-          "3. low": "470.45",
-          "4. close": "480.45",
-          "5. volume": "654321"
-      },
-      "2024-12-20 16:00:00": {
-          "1. open": "465.78",
-          "2. high": "490.12",
-          "3. low": "452.67",
-          "4. close": "493.12",
-          "5. volume": "543210"
-      },
-      "2024-12-20 15:00:00": {
-          "1. open": "450.34",
-          "2. high": "465.78",
-          "3. low": "430.12",
-          "4. close": "465.78",
-          "5. volume": "432109"
-      },
-      "2024-12-20 14:00:00": {
-          "1. open": "530.12",
-          "2. high": "540.45",
-          "3. low": "490.34",
-          "4. close": "450.34",
-          "5. volume": "321098"
-      },
-      "2024-12-20 13:00:00": {
-          "1. open": "510.67",
-          "2. high": "530.12",
-          "3. low": "500.12",
-          "4. close": "530.12",
-          "5. volume": "210987"
-      },
-      "2024-12-20 12:00:00": {
-          "1. open": "540.34",
-          "2. high": "550.45",
-          "3. low": "505.78",
-          "4. close": "510.67",
-          "5. volume": "109876"
-      },
-      "2024-12-20 11:00:00": {
-          "1. open": "522.45",
-          "2. high": "540.78",
-          "3. low": "510.23",
-          "4. close": "540.34",
-          "5. volume": "98765"
-      },
-      "2024-12-20 10:00:00": {
-          "1. open": "490.12",
-          "2. high": "525.45",
-          "3. low": "480.67",
-          "4. close": "522.45",
-          "5. volume": "87654"
-      }
-  }
-};
-
 
 export default function Chart({ chartConfig }: IChartProps) {
   const chartRef = useRef();
@@ -108,12 +18,12 @@ export default function Chart({ chartConfig }: IChartProps) {
     const fetchMarketData = async () => {
       try {
         const API_KEY = process.env.NEXT_PUBLIC_ALPHA_VANTAGE_API_KEY;
-        const response = await axios.get(
-            `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${chartConfig.symbol}&interval=60min&apikey=${API_KEY}`
-          );
-        const data = response.data["Time Series (60min)"];
-        // const data = res["Time Series (60min)"];
-console.log(response)
+        // const response = await axios.get(
+        //     `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${chartConfig.symbol}&interval=60min&apikey=${API_KEY}`
+        //   );
+        // const data = response.data["Time Series (60min)"];
+        const data = tslaRes["Time Series (60min)"];
+
         const parsedData = Object.keys(data).map((date) => ({
             date: new Date(date),
             price: parseFloat(data[date]["4. close"]),
